c7598c2395936724a1153235856e8ba6bd65fe31
Austin Nason <austin.nason@schrodinger.com>
2024-01-29 13:08:10 -0800

    init

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,20 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "visual-studio-code"
+
+  # Communication Tools
+  "discord"
+  "notion"
+  "slack"
+  "telegram"
+  "zoom"
+
+  # Productivity Tools
+  "raycast"
+
+  # Browsers
+  "google-chrome"
+]
