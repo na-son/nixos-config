c1acb86dbd877395cc560ea6159094da6a3b432d
Austin Nason <austin.nason@schrodinger.com>
2024-03-20 16:35:09 -0700

    nixfmt

--- modules/darwin/files.nix
+++ modules/darwin/files.nix
@@ -5,10 +5,10 @@ let
   xdg_dataHome = "${config.users.users.${user}.home}/.local/share";
   xdg_stateHome = "${config.users.users.${user}.home}/.local/state";
 in {
-# tfenv is picky about ggrep being present on osx
-"${xdg_dataHome}/bin/ggrep" = {
-  source = "${pkgs.gnugrep}/bin/grep";
-  executable = true;
-};
+  # tfenv is picky about ggrep being present on osx
+  "${xdg_dataHome}/bin/ggrep" = {
+    source = "${pkgs.gnugrep}/bin/grep";
+    executable = true;
+  };
 
 }
