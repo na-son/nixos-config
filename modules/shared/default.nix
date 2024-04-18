3e8a8d9a48be6d1d7fa68ad8e2745ba1fdbd78ce
Austin Nason <austin.nason@schrodinger.com>
2024-04-18 10:31:16 -0400

    remove overlays and unnecessary let{}

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,15 +1,6 @@
-{
-  config,
-  pkgs,
-  nur,
-  ...
-}:
+{ config, pkgs, ... }:
 
-let
-  emacsOverlaySha256 = "06413w510jmld20i4lik9b36cfafm501864yq8k4vxl5r4hn0j0h";
-in
 {
-
   nixpkgs = {
     config = {
       allowUnfree = true;
@@ -17,15 +8,5 @@ in
       allowInsecure = false;
       allowUnsupportedSystem = true;
     };
-
-    # overlays = [ nur.overlay];
-    # Apply each overlay found in the /overlays directory
-    #let path = ../../overlays;
-    #in with builtins;
-    #map (n: import (path + ("/" + n))) (filter (n:
-    #  match ".*\\.nix" n != null
-    #  || pathExists (path + ("/" + n + "/default.nix")))
-    #  (attrNames (readDir path)))
-    # ;
   };
 }
