52b34c9580b2d238daf38d2815509d2306ccbc9e
Austin Nason <austin.nason@schrodinger.com>
2024-12-17 11:28:49 -0800

    flake update

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -27,8 +27,8 @@ with pkgs;
   #pinentry
 
   # Cloud-related tools and SDKs
-  docker
-  docker-compose
+  #docker
+  #docker-compose
   #awscli
   #qemu
   #podman
@@ -48,7 +48,7 @@ with pkgs;
 
   # Text and terminal utilities
   htop
-  iftop
+  #iftop
   jq
   tree
   tmux
