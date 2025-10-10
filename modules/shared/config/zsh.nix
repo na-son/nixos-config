02283d25b84e70ab85c96d7da66c31e5d0881fde
Austin Nason <austin.nason@schrodinger.com>
2025-10-10 16:08:27 -0700

    move zsh to separate file

---
+++ modules/shared/config/zsh.nix
@@ -0,0 +1,25 @@
+{...}: {
+  enable = true;
+  history = {
+    append = true;
+    ignoreAllDups = true;
+    ignorePatterns = [
+      "cd"
+      "ls"
+      "pwd"
+    ];
+  };
+
+  shellAliases = {
+    cat = "bat";
+    ls = "ls --color=auto";
+  };
+
+  sessionVariables = {
+    EDITOR = "nvim";
+
+    # tfenv stuff
+    TFENV_CONFIG_DIR = "$HOME/.local/share/tfenv";
+    PATH = "$HOME/.tfenv/bin:$PATH";
+  };
+}
--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -5,30 +5,7 @@
   inputs,
   ...
 }: {
-  zsh = {
-    enable = true;
-    history = {
-      append = true;
-      ignoreAllDups = true;
-      ignorePatterns = [
-        "cd"
-        "ls"
-        "pwd"
-      ];
-    };
-
-    shellAliases = {
-      cat = "bat";
-      ls = "ls --color=auto";
-    };
-
-    sessionVariables = {
-      EDITOR = "nvim";
-      # tfenv stuff
-      TFENV_CONFIG_DIR = "$HOME/.local/share/tfenv";
-      PATH = "$HOME/.tfenv/bin:$PATH";
-    };
-  };
+  zsh = import ./config/zsh.zix;
 
   ghostty = {
     enable = true;
