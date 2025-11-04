5d908c15b72b909150af00ce8a244d27db9abad5
Austin Nason <austin.nason@schrodinger.com>
2025-11-04 12:20:30 -0800

    fix nixos.org substituter

--- home/modules/cachix.nix
+++ home/modules/cachix.nix
@@ -8,6 +8,7 @@ _: {
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "na-son.cachix.org-1:CM2NirYn93VKnwoRppqqwbb6XjulYKRTcHsAbVyEpcQ="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 }
