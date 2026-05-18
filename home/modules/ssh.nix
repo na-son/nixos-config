2d2abde485e177f3b90df8fc7bbda06930e62b28
Austin Nason <austin.nason@schrodinger.com>
2026-05-18 15:21:54 -0700

    use work opencode config

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -7,7 +7,7 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks."*" = {
+    settings."*" = {
       forwardAgent = false;
       addKeysToAgent = "no";
       compression = false;
