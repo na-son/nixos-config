2976e09a4ce037bfb3603a3e358a77b906290c82
Austin Nason <austin.nason@schrodinger.com>
2026-05-18 18:51:46 -0700

    sane defaults

--- home/modules/gemini.nix
+++ home/modules/gemini.nix
@@ -1,13 +1,23 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{...}: {
   programs.sdgr-hm.gemini = {
     enable = true;
     mcp.atlassian = {
       enable = true;
+    };
+    extraSettings = {
+      general.vimMode = true;
+      context.fileFiltering.respectGitIgnore = false;
+      ui.hideBanner = true;
+      #ui.errorVerbosity = "full";
+      #ui.inlineThinkingMode = "full";
+      tools.shell.inactivityTimeout = 600;
+      security.disableYoloMode = false;
+      ui.footer.showLabels = false;
+      #experimental = {
+      #  adk.agentSessionInteractiveEnabled = true;
+      #  adk.agentSessionNoninteractiveEnabled = true;
+      #  adk.agentSessionSubagentEnabled = true;
+      #};
     };
   };
 }
